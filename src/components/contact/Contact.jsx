95ab25be6d435503595d158b9eaa1b04e689b112
Muhoza John <muhozajohn250@gmail.com>
2023-09-11 12:36:02 +0200

    Adding Contact Form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import maap from "../../assets/images/map.jpg"
 
 const Contact = () => {
   return (
@@ -8,6 +8,42 @@ const Contact = () => {
           <h1 className="text-white font-semibold text-5xl">Contact</h1>
         </div>
       </div>
+      <section>
+        <div className="container grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <div className="w-full h-1/2 lg:h-[90vh] overflow-hidden ">
+            <img src={maap} alt="" className="rounded-xl w-full h-full " />
+          </div>
+          <div className="flex flex-col gap-6 md:mt-[-28rem] lg:mt-0 mt-[-12rem]">
+            <h2 className="text-2xl font-bold">Contact Us</h2>
+            <form action="" className="flex flex-col gap-4">
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="px-3  py-3 bg-transparent border border-solid border-black placeholder-black  "
+              />
+              <input
+                type="text"
+                placeholder="Your Email"
+                className="px-3  py-3 bg-transparent border border-solid border-black placeholder-black  "
+              />
+              <input
+                type="text"
+                placeholder="SubJect"
+                className="px-3  py-3 bg-transparent border border-solid border-black placeholder-black  "
+              />
+              <textarea
+                name=""
+                placeholder="Message"
+                id=""
+                cols="30"
+                rows="10"
+                className="px-3  py-3 bg-transparent border border-solid border-black placeholder-black "
+              ></textarea>
+              <div className="btn w-fit cursor-pointer">Send Message</div>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };

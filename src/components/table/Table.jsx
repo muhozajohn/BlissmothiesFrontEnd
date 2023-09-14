cd29ba68cc6496bca153eb4ff524b623a889b940
Muhoza John <muhozajohn250@gmail.com>
2023-09-14 17:39:07 +0200

    Adding UserCustomer Page

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -9,56 +9,56 @@ const Table = () => {
             <h2 className="text-2xl font-bold">Make Reservation</h2>
             <form className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full">
               <div className="flex flex-col gap-2">
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm font-semibold">
                   Firstname
                 </label>
                 <input
                   type="text"
                   placeholder="Firstname"
-                  className="rounded-xl border border-black px-3 py-3 w-full"
+                  className="py-3 px-3 border border-solid border-gray-500 rounded-sm bg-transparent placeholder-black outline-none active:outline-none text-sm  w-full"
                 />
               </div>
               <div className="flex flex-col gap-2">
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm font-semibold">
                   Lastname
                 </label>
                 <input
                   type="text"
                   placeholder="Lastname"
-                  className="rounded-xl border border-black px-3 py-3 w-full"
+                  className="py-3 px-3 border border-solid border-gray-500 rounded-sm bg-transparent placeholder-black outline-none active:outline-none text-sm  w-full"
                 />
               </div>
               <div className="flex flex-col gap-2">
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm font-semibold">
                   Phone
                 </label>
                 <input
                   type="text"
-                  placeholder="phone"
-                  className="rounded-xl border border-black px-3 py-3 w-full"
+                  placeholder="Phone"
+                  className="py-3 px-3 border border-solid border-gray-500 rounded-sm bg-transparent placeholder-black outline-none active:outline-none text-sm  w-full"
                 />
               </div>
               <div className="flex flex-col gap-2">
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm font-semibold">
                   Date
                 </label>
                 <input
                   type="date"
                   placeholder="phone"
-                  className="rounded-xl border border-black px-3 py-3 w-full"
+                  className="py-3 px-3 border border-solid border-gray-500 rounded-sm bg-transparent placeholder-black outline-none active:outline-none text-sm  w-full"
                 />
               </div>
               <div className="flex flex-col gap-2">
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm font-semibold">
                   Time
                 </label>
                 <select
                   name=""
                   id=""
                   placeholder="phone"
-                  className="rounded-xl border border-black px-3 py-3 w-full"
+                  className="py-3 px-3 border border-solid border-gray-500 rounded-sm bg-transparent placeholder-black outline-none active:outline-none text-sm  w-full"
                 >
-                  <option className="text-xl font-bold">Time</option>
+                  <option className="text-sm font-bold">Time</option>
                   <option>12:00 am</option>
                   <option>12:30 am</option>
                   <option>1:00 am</option>
@@ -67,7 +67,7 @@ const Table = () => {
                 </select>
               </div>
               <div className="flex flex-col gap-2">
-                <label htmlFor="" className="text-sm">
+                <label htmlFor="" className="text-sm font-semibold">
                   Person
                 </label>
                 {/* <input type="date" placeholder="phone" className="rounded-xl border border-black px-3 py-3 w-full" /> */}
@@ -75,9 +75,9 @@ const Table = () => {
                   name=""
                   id=""
                   placeholder="phone"
-                  className="rounded-xl border border-black px-3 py-3 w-full"
+                  className="py-3 px-3 border border-solid border-gray-500 rounded-sm bg-transparent placeholder-black outline-none active:outline-none text-sm  w-full"
                 >
-                  <option className="text-xl font-bold">Person</option>
+                  <option className="text-sm font-bold">Person</option>
                   <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -86,7 +86,7 @@ const Table = () => {
                 </select>
               </div>
             </form>
-            <div className="btn py-4 cursor-pointer">Make Reservation</div>
+            <div className="btn py-3 cursor-pointer rounded-sm">Make Reservation</div>
           </div>
         </div>
       </div>

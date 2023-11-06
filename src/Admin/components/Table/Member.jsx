c9bc9ab1e33f68a71ceff69c889a58ac0ec076be
Muhoza John <muhozajohn250@gmail.com>
2023-11-06 11:54:54 +0200

    fetching customer and user

--- src/Admin/components/Table/Member.jsx
+++ src/Admin/components/Table/Member.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PulseLoader from "react-spinners/PulseLoader";
 const Member = () => {
+  const [loading, setIsloading] = useState(false);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getMember = async () => {
+      try {
+        setIsloading(true);
+        const userData = await axios.get(
+          "https://blissmothies.onrender.com/blissmothies/users/read"
+        );
+        const response = await userData.data.data;
+
+        if (response) setIsloading(false);
+
+        // Filter the data to include only items with the role of "user"
+        const filteredUsers = response.filter((user) => user.role === "user");
+        setUsers(filteredUsers);
+      } catch (error) {
+        setIsloading(false);
+        console.error("Error fetching data: ", error);
+      }
+    };
+    getMember();
+  }, []);
   return (
     <div className="flex flex-col">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -13,60 +37,66 @@ const Member = () => {
                     #PID
                   </th>
                   <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Customer
+                    User Profile
                   </th>
                   <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Product Owner
+                    FullName
                   </th>
                   <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Total Items
+                    email
                   </th>
                   <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Delivery Date
+                    Gender
                   </th>
-                  <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Delivery Time
-                  </th>
+
                   <th scope="col" className="whitespace-nowrap px-6 py-4">
                     Created On
                   </th>
                   <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Address
-                  </th>
-                  <th scope="col" className="whitespace-nowrap px-6 py-4">
-                    Status
+                    Role
                   </th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-300">
-                  <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                  <td className="whitespace-nowrap px-6 py-4">Zxus</td>
-                  <td className="whitespace-nowrap px-6 py-4">keller</td>
-                  <td className="whitespace-nowrap px-6 py-4">20</td>
-                  <td className="whitespace-nowrap px-6 py-4">01/ 02 /2023</td>
-                  <td className="whitespace-nowrap px-6 py-4 font-medium">
-                    {" "}
-                    14:300 pm{" "}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">01/ 02 /2023</td>
-                  <td className="whitespace-nowrap px-6 py-4">Kigali</td>
-                  <td className="whitespace-nowrap px-6 py-4">Completed</td>
-                </tr>
-                <tr className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-300">
-                  <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                  <td className="whitespace-nowrap px-6 py-4">Muhoza</td>
-                  <td className="whitespace-nowrap px-6 py-4">keller</td>
-                  <td className="whitespace-nowrap px-6 py-4">20</td>
-                  <td className="whitespace-nowrap px-6 py-4">01/ 02 /2023</td>
-                  <td className="whitespace-nowrap px-6 py-4 font-medium">
-                    {" "}
-                    14:300 pm{" "}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">01/ 02 /2023</td>
-                  <td className="whitespace-nowrap px-6 py-4">Kigali</td>
-                  <td className="whitespace-nowrap px-6 py-4">Progres</td>
-                </tr>
+                {users.length ? (
+                  users.map((item, index) => (
+                    <tr
+                      key={index}
+                      className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-300"
+                    >
+                      <td className="whitespace-nowrap px-6 py-4 font-medium">
+                        {index + 1}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        <img
+                          src={item.userProfile}
+                          alt=""
+                          className="h-8 rounded-md"
+                        />
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {item.fullName}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {item.email}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {item.gender}
+                      </td>
+
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {item.updatedAt}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
+                        {item.role}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <div className=" mt-5 flex justify-center items-center w-full text-center  ">
+                    <PulseLoader color="#F06C05" loading={loading} size={10} />
+                  </div>
+                )}
               </tbody>
             </table>
           </div>
@@ -74,6 +104,6 @@ const Member = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Member
+export default Member;
